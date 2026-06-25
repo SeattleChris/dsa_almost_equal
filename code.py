edcac3d6a9a935537cac75cb3fcd9aef48ae6534
Chris Chapman <Chris@SeattleCode.dev>
2026-06-25 16:17:00 -0700

    Three solutions and timing for test 3 results.

--- code.py
+++ code.py
@@ -1,25 +1,29 @@
-import math
-import os
-import random
-import re
-import sys
+# import os
 from functools import cache
 from itertools import combinations
 
 
-def cache_solve(h, target, queries):
-    """Lookup or compute & store technique."""
+def solve(h, target, queries):
+    """3min 8sec. Lookup or compute & cache technique."""
 
     @cache
-    def _helper(a, b, target):
+    def _helper(a, b):
         return abs(h[a] - h[b]) <= target
 
     combos = (combinations(range(j,k+1), 2) for j, k in queries)
-    return [sum(_helper(a, b, target) for a, b in pairs) for pairs in combos]
+    return [sum(_helper(a, b) for a, b in pairs) for pairs in combos]
 
 
-def solve(h, target, queries):
-    """Determine all valid pairs amongst those in the queries."""
+def stored_solve(h, target, queries):
+    """3min 57sec. Lookup or compute & store technique."""
+    data = {}
+    combos = (combinations(range(j,k+1), 2) for j, k in queries)
+    return [sum(data.setdefault((a, b), abs(h[a] - h[b]) <= target) for a, b in pairs) for pairs in combos]
+
+
+
+def find_valid_solve(h, target, queries):
+    """4min 33sec. Determine all valid pairs amongst those in the queries."""
     pairs = {pair for j, k in queries for pair in combinations(range(j,k+1), 2)}
     valid_pairs = {(a, b,) for a, b in pairs if abs(h[a] - h[b]) <= target}
     q_combos = (combinations(range(j,k+1), 2) for j, k in queries)
@@ -30,13 +34,13 @@ def solve(h, target, queries):
 if __name__ == '__main__':
     first_multiple_input = input().rstrip().split()
     n = int(first_multiple_input[0])
-    k = int(first_multiple_input[1])
+    target = int(first_multiple_input[1])
     h = list(map(int, input().rstrip().split()))
     q = int(input().strip())
     queries = []
     for _ in range(q):
         queries.append(list(map(int, input().rstrip().split())))
-    result = solve(h, k, queries)
+    result = solve(h, target, queries)
     output = '\n'.join(map(str, result))
     print(output)
     # fptr = open(os.environ['OUTPUT_PATH'], 'w')
