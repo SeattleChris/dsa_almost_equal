e88fde7acc4fb1ac54887c051425b4df4e5c1fa6
Chris Chapman <Chris@SeattleCode.dev>
2026-08-14 17:42:36 -0700

    Various techniques for a solution.

--- code.py
+++ code.py
@@ -1,9 +1,10 @@
 # import os
 from functools import cache
-from itertools import combinations
+from itertools import combinations, chain
+from collections import defaultdict
 
 
-def solve(h, target, queries):
+def cache_solve(h, target, queries):
     """3min 8sec. Lookup or compute & cache technique."""
 
     @cache
@@ -21,14 +22,236 @@ def stored_solve(h, target, queries):
     return [sum(data.setdefault((a, b), abs(h[a] - h[b]) <= target) for a, b in pairs) for pairs in combos]
 
 
-
-def find_valid_solve(h, target, queries):
+def find_valid_combo_solve(h, target, queries):
     """4min 33sec. Determine all valid pairs amongst those in the queries."""
     pairs = {pair for j, k in queries for pair in combinations(range(j,k+1), 2)}
     valid_pairs = {(a, b,) for a, b in pairs if abs(h[a] - h[b]) <= target}
     q_combos = (combinations(range(j,k+1), 2) for j, k in queries)
     return [sum(pair in valid_pairs for pair in q_pairs) for q_pairs in q_combos]
 
+
+def cache_seq_solve(h, target, queries):
+    """2min 44sec. Using sequence for pairing and cache of pair results."""
+    @cache
+    def _helper(j, k):
+        return abs(h[j] - h[k]) <= target
+
+    return [sum(_helper(j, k) for j in range(a, b) for k in range(j + 1, b + 1)) for a, b in queries]
+
+
+def seq_set_solve(h, target, queries):
+    """2min 3sec. Sets of valid paired indexes."""
+    paired = [set() for _ in h]  # all indices that will be paired with a given index
+    for a, b in queries:
+        for j in range(a, b):
+            paired[j].update(range(j+1, b+1))
+    valid = [
+        {idx for idx in group if abs(h[pos] - h[idx]) <= target}
+        for pos, group in enumerate(paired)
+        ]
+    return [sum(j in valid[i] for i in range(a, b) for j in range(i + 1, b + 1)) for a, b in queries]
+
+
+def valid_pair_solve(h, target, queries):
+    """3min 6sec (combinations 3min 11sec). Sets of valid paired indexes."""
+    paired = [set() for _ in h]  # all indices that will be paired with a given index
+    for a, b in queries:
+        for j in range(a, b):
+            paired[j].update(range(j+1, b+1))
+    valid = {
+        (pos, idx)
+        for pos, group in enumerate(paired)
+        for idx in group
+        if abs(h[pos] - h[idx]) <= target
+        }
+    return [
+        sum(
+            (i, j) in valid
+            for i in range(a, b)
+            for j in range(i + 1, b + 1)
+            )
+        for a, b in queries
+        ]
+
+
+def setdefault_solve(h, target, queries):
+    """2min 36sec. Use Dict.setdefault of paired indexes."""
+    matrix = [dict() for _ in h]
+    results = []
+    for a, b in queries:
+        score = 0
+        for j in range(a, b):
+            for k in range(j+1, b+1):
+                score += matrix[j].setdefault(k, abs(h[j] - h[k]) <= target)
+        results.append(score)
+    return results
+
+
+def fly2_solve(h, target, queries):
+    """2min 1sec. On the fly determine if already known or compute value."""
+    matrix = [[-1] * len(h) for _ in h]
+    results = []
+    for a, b in queries:
+        score = 0
+        for j in range(a, b):
+            for k in range(j+1, b+1):
+                matrix[j][k] = (res := matrix[j][k] if matrix[j][k] != -1 else abs(h[j] - h[k]) <= target)
+                score += res
+        results.append(score)
+    return results
+
+
+def matrix_chain_solve(h, target, queries):
+    """1min 46sec. Matrix of paired indexes."""
+    matrix = [[0] * len(h) for _ in h]
+    paired = [[] for _ in h]  # all indices that will be paired with a given index
+    for a, b in queries:
+        for j in range(a, b):
+            paired[j].append(range(j+1, b+1))  # += [x for x in range(j +1, b+1)]
+    for pos, group in enumerate(paired):
+        for idx in set(chain.from_iterable(group)):
+            matrix[pos][idx] = abs(h[pos] - h[idx]) <= target
+    return [sum(matrix[i][j] for i in range(a, b) for j in range(i + 1, b + 1)) for a, b in queries]
+
+
+def fly_no_save_solve(h, target, queries):
+    """1min 55sec, 2min 48sec with cache helper. Matrix of paired indexes."""
+    results = []
+    for a, b in queries:
+        score = 0
+        for pos in range(a, b):
+            for idx in range(pos+1, b+1):
+                score += abs(h[pos] - h[idx]) <= target
+        results.append(score)
+    return results
+
+
+def fly_cache_solve(h, target, queries):
+    """2min 45sec. On the fly with cache for value."""
+    @cache
+    def _helper(j, k):
+        return abs(h[j] - h[k]) <= target
+
+    return [
+        sum(
+            _helper(j, k)
+            for j in range(a, b)
+            for k in range(j+1, b+1)
+            )
+        for a, b in queries
+        ]
+
+
+def matrix_set_solve(h, target, queries):
+    """1min 37sec. Matrix of paired indexes."""
+    matrix = [[0] * len(h) for _ in h]
+    paired = [set() for _ in h]  # all indices that will be paired with a given index
+    for a, b in queries:
+        for j in range(a, b):
+            paired[j].update(range(j+1, b+1))
+    for pos, group in enumerate(paired):
+        for idx in group:
+            matrix[pos][idx] = abs(h[pos] - h[idx]) <= target
+    return [sum(matrix[i][j] for i in range(a, b) for j in range(i + 1, b + 1)) for a, b in queries]
+
+
+def fly_solve(h, target, queries):
+    """1min 29sec (16s lookup, 46s if-else, 25s no save) On the fly per query."""
+    matrix = [[-1] * len(h) for _ in h]
+    results = []
+    for a, b in queries:
+        score = 0
+        for j in range(a, b):
+            for k in range(j+1, b+1):
+                if (res := matrix[j][k]) == -1:
+                    matrix[j][k] = (res := abs(h[j] - h[k]) <= target)
+                score += res
+        results.append(score)
+    return results
+
+class Interval:
+    def __init__(self, start: int, end: int, members: set[int]):
+        self.start = start
+        self.end = end
+        self.members = members
+
+    @classmethod
+    def origin(cls, start: int, end: int, *members: list[int]):
+        tmp = cls(start, end, set(members))
+        tmp._start = tmp.start
+        tmp._end = tmp._end
+        tmp._origin: int = members.pop()
+        return tmp
+
+    def __lt__(self, other):
+        return self.start < other.start
+
+    def __union__(self, other):
+        a, b = (self, other) if self.start < other.start else (other, self)
+        if a.end <= b.start:  # No overlap, return sorted intervals.
+            return (a, b)
+        if a.end >= b.end:  # 'b' is completely within 'a' interval :: a_s ... b_s ... b_e ... a_e
+            last = Interval(b.end, a.end, a.members)
+            a.end = b.start
+            b.members |= a.members
+            return (a, b, last)
+        mid = Interval(b.start, a.end, b.members | a.members)
+        b.start = a.end
+        a.end = b.start
+        return (a, mid, b)  # b starts in a, b extends beyond a :: a_s ... b_s ... a_e ... b_e
+
+
+def merge_intervals(intervals: list[tuple[int, int]]) -> (list[Interval]):
+    # 1. Sort intervals by their start values, recording their original indices
+    intervals = sorted((Interval.origin(s, e, idx) for idx, (s, e) in enumerate(intervals)))
+    merged, pre = [], intervals[0]
+    for cur in intervals[1:]:
+        *ext, pre = pre | cur
+        merged.extend(ext)
+    merged.append(pre)
+    return merged
+
+
+def overlap_solve(h, target, queries):
+    """1min 29sec. Determine overlapping queries to reduce number of pairs to compute."""
+    result = [0 for _ in queries]
+    intervals: list[Interval] = merge_intervals(queries)
+    for curr in intervals:
+        a, b = curr.start, curr.end
+        score = sum(abs(h[j] - h[k]) <= target for j in range(a, b) for k in range(j+1, b+1))
+        for idx in curr.members:
+            result[idx] += score
+        if hasattr(curr, '_origin') and (c := curr.end) <= (d := curr._end) \
+            and ((a := curr._start) != (b := curr.start) or c != d):
+            score = sum(abs(h[j] - h[k]) <= target for j in range(a, b) for k in range(b+1, d+1))
+            score += sum(abs(h[j] - h[k]) <= target for j in range(b, c) for k in range(c+1, d+1))
+            result[curr._origin] += score
+    return result
+
+
+def match_solve(h, target, queries):
+    """4min 45sec. Collect all pairs to compute along with query source."""
+    matrix = [[0] * len(h) for _ in h]
+    source = [defaultdict(list) for _ in h]  # query sources for a given pair
+    paired = [set() for _ in h]  # all indices that will be paired with a given index
+    results = [0 for _ in queries]
+    for num, (a, b) in enumerate(queries):
+        for j in range(a, b):
+            paired[j].update(range(j+1, b+1))
+            # source[j]
+            for idx in range(j+1, b+1):
+                source[j][idx].append(num)
+    for pos, group in enumerate(paired):
+        for idx in group:
+            if abs(h[pos] - h[idx]) <= target:
+                matrix[pos][idx] = 1
+                for query in source[pos][idx]:
+                    results[query] += 1
+    return results
+
+
+def solve(h, target, queries):
+    return fly_solve(h, target, queries)
 
 
 if __name__ == '__main__':
